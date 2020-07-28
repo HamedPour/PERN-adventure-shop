138e372f8b371f0283d659b041f54ab25ef5ae76
HamedPour <hamed_k.pour@hotmail.com>
2020-07-28 17:01:34 +0100

    setup hashing

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,6 +1,7 @@
-// database pool import here
+const pool = require("../models/db");
 const config = require("../config/config");
 const jwt = require("jsonwebtoken");
+const hashing = require("../models/hashing");
 
 function jwtSignAdventurer(anAdventurer) {
   const ONE_WEEK = 60 * 60 * 24 * 7;
@@ -11,13 +12,21 @@ function jwtSignAdventurer(anAdventurer) {
 
 module.exports = {
   // signUP
-  signup(req, res) {
-    const adventurer = {
-      email: req.body.email,
-      password: req.body.password,
-    };
-    const token = jwtSignAdventurer(adventurer);
-    res.json({ adventurer, token });
+  async signup(req, res) {
+    try {
+      // HASH PASSWORD FIRST!
+      const hashedPassword = await hashing(req.body.password);
+      console.log(typeof hashedPassword);
+      const dbSingupResponse = await pool.query(
+        "INSERT INTO adventurer (email, password) VALUES ($1, $2) RETURNING *",
+        [req.body.email, hashedPassword]
+      );
+      const adventurer = dbSingupResponse.rows[0];
+      const token = jwtSignAdventurer(adventurer);
+      res.json({ adventurer, token });
+    } catch (err) {
+      console.error(err);
+    }
   },
   // login
   // logout
